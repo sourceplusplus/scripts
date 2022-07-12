2508f9472c31844ba76e003b090f9c07e3b6e3db
Brandon Fergerson <bfergerson@apache.org>
2022-07-12 12:37:41 +0400

    Create install-boot-demo.sh

---
+++ install-boot-demo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#check docker
+if ! command -v docker > /dev/null; then
+  apt install docker.io
+fi
+
+#check java
+if ! command -v java > /dev/null; then
+  apt install default-jre
+fi
+
+#install live-platform
+git clone --recurse-submodules https://github.com/sourceplusplus/live-platform
+cd live-platform
+git checkout 0dceb9f6c0acb25184bc18f720e68a66e5dfad92
+git submodule update --init --recursive
+
+./gradlew -p platform assembleUp
