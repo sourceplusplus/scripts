2c1a65dbc2918c9b2ba9bad39e95f30beb5a92f5
Brandon Fergerson <bfergerson@apache.org>
2022-07-21 10:48:32 +0400

    boot latest

--- install-boot-demo.sh
+++ install-boot-demo.sh
@@ -18,7 +18,7 @@ fi
 #install live-platform
 git clone --recurse-submodules https://github.com/sourceplusplus/live-platform
 cd live-platform
-git checkout 0dceb9f6c0acb25184bc18f720e68a66e5dfad92
-git submodule update --init --recursive
+#git checkout 0dceb9f6c0acb25184bc18f720e68a66e5dfad92
+#git submodule update --init --recursive
 
 ./gradlew -p platform assembleUp
